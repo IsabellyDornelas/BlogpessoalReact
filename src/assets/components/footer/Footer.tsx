d34021fd46dfdd6c926440b3084ad400b36498b0
Isabelly Dornelas <isabelly.dornelas123@gmail.com>
2024-08-21 11:54:54 -0300

    ajustando

--- src/assets/components/footer/Footer.tsx
+++ src/assets/components/footer/Footer.tsx
@@ -14,9 +14,9 @@ function Footer() {
   if(usuario.token !== '') {
     footerComponent = (
       <>
-        <div className="flex justify-center bg-red-900 text-white">
+        <div className="flex justify-center bg-pink-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>© 2024 Blog Pessoal, Inc | Autora Yanca {data}</p>
+            <p className='text-xl font-bold'>© 2024 Blog Pessoal, Inc | Isabelly Dornelas {data}</p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
               <LinkedinLogo size={48} weight='bold' />
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -8,11 +8,11 @@ import ModalPostagem from '../../assets/components/postagens/modalPostagem/Modal
 function Home() {
   return (
       <>
-      <div className="bg-red-400 flex justify-center">
+      <div className="bg-pink-400 flex justify-center">
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
-            <p className='text-xl'>Seu espaço para criatividade!</p>
+            <p className='text-xl'>Seu lugar é aqui!</p>
 
             <div className="flex justify-around gap-4">
             <ModalPostagem />
